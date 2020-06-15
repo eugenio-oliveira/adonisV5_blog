91280e4f0f593273d00723f30e04232c067e7a9e
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2020-06-15 17:06:45 -0300

    Creating a Blog with Adonis5

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,27 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Post from 'App/Models/Post'
+
+
+export default class PostsController {
+
+  public async index() {
+
+    const posts = await Post.all()
+
+    return posts
+
+  }
+
+
+  public async create({ request }: HttpContextContract) {
+
+    const data = request.only(['title', 'content'])
+
+    const post = await Post.create(data)
+
+    return post
+
+  }
+
+}
